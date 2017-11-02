4b4a92be8ce13c36a02f7ab2d4f9c11b5f9fa5a3
erickmx <erickblanc96@gmail.com>
2017-11-02 14:09:46 -0600

    creacion del schema chofer

--- models/chofer.ts
+++ models/chofer.ts
@@ -5,3 +5,13 @@ telefono
 status
 
 */
+
+import { Schema, model } from 'mongoose'
+
+const choferSchema = new Schema({
+  name: { type: String, required: true },
+  phoneNumber: { type: Number, required: true },
+  status: { type: String, required: true, default: "activo" }
+})
+
+export default model('Chofer', choferSchema)
