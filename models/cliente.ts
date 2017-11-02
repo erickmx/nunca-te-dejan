63ac42d46007a4fb08991ebd18ce744cb82c23bd
erickmx <erickblanc96@gmail.com>
2017-11-02 16:43:02 -0600

    creacion del schema client

--- models/cliente.ts
+++ models/cliente.ts
@@ -11,3 +11,54 @@ saldo
 status
 
 */
+
+// regex para idioma latino
+// var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// regex oficial
+// var regOficial = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+
+import { Schema, model } from 'mongoose'
+
+const validEmail = (email:string):boolean => {
+  return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email)
+}
+
+const clientSchema = new Schema({
+  rfc: {
+    type: String,
+    minlength: [12, 'RFC too short to save'],
+    maxlength: [13, 'RFC too long to save'],
+    required: [true, "RFC it's necesary"],
+    unique: [true, 'RFC shoud be unique']
+  },
+  name: {
+    type: String,
+    required: [true, 'name is required']
+  },
+  direction: {
+    type: String
+  },
+  phoneNumber: {
+    type: Number
+  },
+  email: {
+    validate: {
+      validEmail,
+      message: 'email invalid'
+    }
+  },
+  vehiculosacargo: {
+    type: Number,
+    default: 0
+  },
+  saldo: {
+    type: Number
+  },
+  status: {
+    type: String,
+    default: 'activo'
+  }
+})
+
+export default model('Client', clientSchema)
