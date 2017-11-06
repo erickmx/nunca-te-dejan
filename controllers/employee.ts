4ed25ab6860c38183257c3cd7189dabb741bf48a
erickmx <erickblanc96@gmail.com>
2017-11-06 11:00:32 -0600

    terminado el controller de employee

--- controllers/employee.ts
+++ controllers/employee.ts
@@ -21,6 +21,73 @@ export const getEmployeeByRFC = (req:Request, res:Response, next:NextFunction) :
 
 export const postEmployee = (req:Request, res:Response, next:NextFunction) :void => {
 
-  
+  let {
+    rfc,
+    name,
+    addres,
+    phoneNumber,
+    email,
+    area,
+    status
+  } = req.body
+
+  if (!rfc) {
+    res.status(402).json({err: 'RFC is requred'})
+    return
+  }
+
+  if (!name) {
+    res.status(402).json({err: 'Name is required'})
+    return
+  }
+
+  if (!addres) {
+    addres = ''
+  }
+  if (!email) {
+    res.status(402).json({err: 'Email is required'})
+    return
+  }
+
+  if (!area) {
+    res.status(402).json({err: 'Area is required'})
+    return
+  }
+
+  if (!status) {
+    status = 'activo'
+  }
+
+  const newEmployee = new Employee({
+    rfc,
+    name,
+    addres,
+    phoneNumber,
+    email,
+    area,
+    status
+  })
+
+  newEmployee.save()
+    .then(employee => res.status(200).json({employee}))
+    .catch(err => res.status(500).json({err}))
+
+}
+
+const updateEmployee = (req:Request, res:Response, next:NextFunction) : void => {
+
+  const rfc = req.params.rfc
+  Employee.findOneAndUpdate({rfc}, req.body)
+    .then(employee => res.status(200).json({employee}))
+    .catch(err => res.status(500).json({err}))
+
+}
+
+const deleteEmployee = (req:Request, res:Response, next:NextFunction) : void => {
+
+  const rfc = req.params.rfc
+  Employee.findOneAndRemove({rfc})
+    .then(employee => res.status(200).json({employee}))
+    .catch(err => res.status(500).json({err}))
 
 }
