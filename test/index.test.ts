0e6ba410739aa40b1c9fbd35d2c8c2ad0674e6f1
erickmx <erickblanc96@gmail.com>
2017-12-07 17:58:37 -0600

    automatizacion de la transpilacion de typescript gracias a gulp

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,11 @@
 import { expect, assert } from 'chai'
 
-describe('Api unit test', function() {
-  it('should say hello world')
+describe('# Api unit test', function() {
+  it('should say hello world', () => {
+    assert.notEqual("Hola mundo", "Holo munda")
+  })
+  
+  it('should check the type', () => {
+    expect(1).to.be.equals(1)
+  })
 })
